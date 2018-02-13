021bf39f10463c2bfd56297d23f89e94ab056a4b
Alistair McKelvie <alistair.mckelvie@gmail.com>
2018-02-13 23:37:05 +1100

    add file logging

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import logging
 import sys
+import os
 
 from windvisdata.latest_gfs_to_json import latest_gfs_to_json
 
+log_file = os.path.dirname(os.path.realpath(__file__)) + '/wind-vis-data.log'
 
 logging.basicConfig(
     format='%(asctime)-15s %(message)s',
-    stream=sys.stdout,
+    filename=log_file,
     level=logging.INFO)
 
 latest_gfs_to_json()
