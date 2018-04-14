6ccdd3837ca853f8ae7cf7bbe6659c74f8e81345
Alistair <alistair.mckelvie@gmail.com>
2018-04-14 14:58:46 +1000

    add maxWindSpd to cycle.json

--- windvisdata/latest_gfs_to_json.py
+++ windvisdata/latest_gfs_to_json.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 
+import numpy as np
 import pygrib
 
 from . import gfs_download
@@ -16,11 +17,14 @@ def latest_gfs_to_json():
         os.makedirs(JSON_DIR)
     cycle = gfs_download.get_latest_cycle()
     grib_files = []
+    max_wind_spd = 0
     for i, grib_file_path in enumerate(gfs_download.download_cycle(cycle)):
         tau = i * 3
         grib_data = pygrib.open(grib_file_path)
-        u_grib_data = grib_data.select(name='U component of wind')[0]
-        v_grib_data = grib_data.select(name='V component of wind')[0]
+        u_data = grib_data.select(name='U component of wind')[0]['values']
+        v_data = grib_data.select(name='V component of wind')[0]['values']
+        max_wind_speed = max(
+            np.max(np.sqrt(u_data**2 + v_data**2)), max_wind_spd)
 
         data_1_deg = {}
         data_1_deg['uData'] = []
@@ -36,15 +40,13 @@ def latest_gfs_to_json():
                 data_2_deg['vData'].append([])
 
             for y in range(180, -1, -1):
-                data_1_deg['uData'][x].append(
-                    round(float(u_grib_data['values'][y][x])))
-                data_1_deg['vData'][x].append(
-                    round(float(v_grib_data['values'][y][x])))
+                data_1_deg['uData'][x].append(round(float(u_data[y][x])))
+                data_1_deg['vData'][x].append(round(float(v_data[y][x])))
                 if y % 2 == 0:
-                    data_2_deg['uData'][x // 2].append(
-                        round(float(u_grib_data['values'][y][x])))
-                    data_2_deg['vData'][x // 2].append(
-                        round(float(v_grib_data['values'][y][x])))
+                    data_2_deg['uData'][x //
+                                        2].append(round(float(u_data[y][x])))
+                    data_2_deg['vData'][x //
+                                        2].append(round(float(v_data[y][x])))
 
         json_1_deg_path = (f'{JSON_DIR}/'
                            f'gfs_100_{cycle:%Y%m%d_%H%M%S}_{tau:03d}.json')
@@ -59,7 +61,10 @@ def latest_gfs_to_json():
         LOG.info(f'created {json_2_deg_path}')
 
     with open(f'{JSON_DIR}/cycle.json', 'w') as w:
-        json.dump({'cycle': f'{cycle:%Y%m%d_%H%M%S}'}, w)
+        json.dump({
+            'cycle': f'{cycle:%Y%m%d_%H%M%S}',
+            'maxWindSpeed': int(np.ceil(max_wind_spd))
+        }, w)
 
     for f in glob.glob(gfs_download.GRIB_ + '/*'):
         os.remove(f)
